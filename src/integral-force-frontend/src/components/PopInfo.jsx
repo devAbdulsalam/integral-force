83cd8b4cbc1567c369833d6fdef2a122f3ad4c37
devAbdulsalam <ammuftau74@gmail.com>
2024-09-15 15:24:07 +0100

    improve ui

--- src/integral-force-frontend/src/components/PopInfo.jsx
+++ src/integral-force-frontend/src/components/PopInfo.jsx
@@ -2,22 +2,24 @@ const App = () => {
     return (
         <div className="popup-info">
         <h2>Quiz Guide</h2>
-        <span className="info"
-            >1. Choose your answers currently because once picked, you will not be
-            able to change it.</span>
-        <span className="info">2. Beat The Clock (10 seconds)</span>
+        <span className="info">
+            1. Choose your answers currently because once picked, you will not be
+            able to change it.
+        </span>
+        <span className="info">
+            2. Beat The Clock (10 seconds)
+        </span>
         <span className="info">
             3. Focus On The Current Question Before Moving To The Next One.
-            </span >
-
-
-
+        </span >
         <div className="btn-group">
             <button className="info-btn exit-btn" onclick="hideSection()">
                 Exit Quiz
             </button>
-            <a href="#" className="info-btn continue-btn" onclick="showSection()"
-                >Continue</a>        </div>
+            <a href="#" className="info-btn continue-btn" onclick="showSection()">
+                Continue
+            </a>        
+        </div>
     </div> 
     )
 }
